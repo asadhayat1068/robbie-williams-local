b162cf4731548db3f601a15fc7ab4aca9e23e4a2
Asad Hayat <asadhayat.1068@gmail.com>
2023-08-31 14:41:02 +0500

    Refactored hello api to remove warnings

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: "Hello World" });
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({
+    text: "Hello World",
+    method: req.method,
+  });
 };
+export default handler;

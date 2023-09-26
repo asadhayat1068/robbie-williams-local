24de2e827e1310afe46094a21e929373b58d7743
Asad Hayat <asadhayat.1068@gmail.com>
2023-09-26 18:38:00 +0500

    Updated eventbrite webhook as per updated user CRUD

--- pages/helpers/ticket.ts
+++ pages/helpers/ticket.ts
@@ -1,76 +1,65 @@
 import axios from "axios";
 import prisma from "@/lib/prisma";
 import { PRIVATE_API_TOKEN } from "../configs/app";
-import { addUser, getUser, userExists } from "./user";
-import { User } from "../types/User.type";
+import { getUserData, createUser } from "./user";
 import { Order } from "../types/Order.type";
 import { Ticket } from "../types/Ticket.type";
 import { mintTicket } from "./provider";
-import { AddressZero } from "@etherspot/prime-sdk/dist/sdk/common";
+import { ZeroAddress } from "ethers";
 
 export const processTicket = async (data: any) => {
-    // data = JSON.parse('{"config":{"endpoint_url":"https://event-brite.vercel.app/api/ticket","user_id":"1731318251883","action":"order.placed","webhook_id":"11719981"},"api_url":"https://www.eventbriteapi.com/v3/orders/7516439669/"}');
-    const action = data.config.action;
-    const user_id = data.config.user_id;
-    const api_url = data.api_url + "?token=" + PRIVATE_API_TOKEN;
-    if (action === 'order.placed') {
-        const resp = await axios.get(api_url);
-        const ebOrder = resp.data;
-        if (ebOrder.status === 'placed') {
-            // console.log("Order placed", ebOrder);
-            const user: User = {
-                name: ebOrder.name,
-                email: ebOrder.email
-            }
-            const userExist = await userExists(user);
-            let dbUser;
-            if (!userExist) {
-                dbUser = await addUser(user);
-            } else {
-                dbUser = await getUser(user);
-            }
-            // // handle order entry
-            if (!dbUser) {
-                throw new Error("User not found");
-            }
+  // data = JSON.parse('{"config":{"endpoint_url":"https://event-brite.vercel.app/api/ticket","user_id":"1731318251883","action":"order.placed","webhook_id":"11719981"},"api_url":"https://www.eventbriteapi.com/v3/orders/7516439669/"}');
+  const action = data.config.action;
+  const user_id = data.config.user_id;
+  const api_url = data.api_url + "?token=" + PRIVATE_API_TOKEN;
+  if (action === "order.placed") {
+    const resp = await axios.get(api_url);
+    const ebOrder = resp.data;
+    const { name, email } = ebOrder;
+    let user;
+    if (ebOrder.status === "placed") {
+      user = await getUserData(email);
+      if (!user) {
+        user = await createUser({ name, email });
+      }
 
-            const order: Order = {
-                id: ebOrder.id,
-                userId: dbUser.id
-            }
-            const dbOrder = await prisma.order.create({
-                data: {
-                    orderId: order.id,
-                    userId: dbUser.id
-                }
-            });
-            // // handle ticket entry
-            const ticket: Ticket = {
-                ticketId: ebOrder.id,
-                orderId: dbOrder.id,
-                email: ebOrder.email
-            }
+      const order: Order = {
+        id: ebOrder.id,
+        userId: user.id,
+      };
+      const dbOrder = await prisma.order.create({
+        data: {
+          orderId: order.id,
+          userId: user.id,
+        },
+      });
+      // // handle ticket entry
+      const ticket: Ticket = {
+        ticketId: ebOrder.id,
+        orderId: dbOrder.id,
+        email: ebOrder.email,
+      };
 
-            const dbTicket = await prisma.ticket.create({
-                data: ticket
-            });
-            // Mint Ticket, if user has address
-            if (dbUser?.address && dbUser?.address != AddressZero) {
-                mintTicket(dbUser.address,1, 467458, dbUser.name, dbUser.email);
-            }
-
-            return { success: true, message: "Order placed. Minting NFT." };
-
-        } else {
-            return { success: false, error: "invalid action"}
-        }
+      const dbTicket = await prisma.ticket.create({
+        data: ticket,
+      });
+      // Mint Ticket, if user has address
+      if (user?.address && user?.address != ZeroAddress) {
+        mintTicket(user.address, 1, 467458, user.name, user.email);
+        return { success: true, message: "Order placed. Minting NFT." };
+      } else {
+        return { success: true, message: "Order placed. No NFT minted." };
+      }
+    } else {
+      return { success: false, error: "Invalid action" };
     }
-    return { success: false, error: "Order not placed" };
-}
+  }
+  return { success: false, error: "Order not placed" };
+};
 
-// const handleUser = async (user: any) => { 
+// const handleUser = async (user: any) => {
 //     return await userExists(user);
-    
+
 // }
 // {
 //     "costs": {

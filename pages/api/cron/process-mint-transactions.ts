931c5f5a97a8029a81ac7487eac94d5ae7398726
Asad Hayat <asadhayat.1068@gmail.com>
2023-09-27 18:45:01 +0500

    "local works"

--- pages/api/cron/process-mint-transactions.ts
+++ pages/api/cron/process-mint-transactions.ts
@@ -6,5 +6,6 @@ export default async function handler(
   res: NextApiResponse
 ) {
   await saveLog({ check: "check", time: Date.now() });
-  return res.status(200).json({ success: true });
+  res.status(200).json({ success: true });
+  return;
 }

3d69266253c69c858812f8482dc7972c8d93717b
Asad Hayat <asadhayat.1068@gmail.com>
2023-10-11 23:32:00 +0500

    Fixed Bugs

--- components/User/Tickets/Ticket.tsx
+++ components/User/Tickets/Ticket.tsx
@@ -1,22 +1,22 @@
 "use client";
+import { useAuth } from "@/app/Context/store";
 import { claimTicket } from "@/services/user";
 import React from "react";
 
 function Ticket({ ticket }: { ticket: any }) {
+  const { authData } = useAuth();
   const ticketId = ticket.id;
   const txHash = ticket?.mintingQueue[0]?.transactionHash ?? "-";
   const tokenId = ticket?.tokens[0]?.tokenId ?? "-";
 
   const claim = async () => {
-    const res = await claimTicket(ticketId);
+    const res = await claimTicket(ticketId, authData.jwt);
     if (!res.error && res.data.success) {
       alert(res.data.message);
     }
   };
 
-  const transfer = async () => {
-    alert("transfer - not implemented");
-  };
+  const transfer = async () => {};
 
   return (
     <>
@@ -35,24 +35,6 @@ function Ticket({ ticket }: { ticket: any }) {
             <button onClick={claim}>Claim</button>
           )}
         </td>
-      </tr>
-      <tr>
-        {/* Recipient Email */}
-        <div className="float-right">
-          <div className="sm:col-span-4">
-            <label className="block text-sm font-medium leading-6 text-gray-900">
-              Recipient Email:
-            </label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              autoComplete="username"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="Recipient's email address"
-            />
-          </div>
-        </div>
       </tr>
     </>
   );

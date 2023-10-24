9efc920dc2aeefff23ce63591f3e8337eaf4a1e2
Giulio Pezzulli <giulio@aitutor.co.uk>
2023-10-24 18:54:27 +0100

    temporary fix to cors

--- lib/configs/app.ts
+++ lib/configs/app.ts
@@ -1,5 +1,7 @@
 export const PRIVATE_API_TOKEN = process.env.PRIVATE_API_TOKEN;
 export const EVENTBRITE_API_BASE_URI = "https://www.eventbriteapi.com/v3";
 export const API_BASE_URI = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL + "/api"
+  ? process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? "https://robbie-williams-event.vercel.app/api"
+    : "https://" + process.env.NEXT_PUBLIC_VERCEL_URL + "/api"
   : "http://localhost:3000/api";

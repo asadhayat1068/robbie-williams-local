6cd54add85f8dda71bf3987ae43da4a1958378c5
Giulio Pezzulli <giulio@aitutor.co.uk>
2023-10-24 17:10:54 +0100

    fixed api url

--- lib/configs/app.ts
+++ lib/configs/app.ts
@@ -1,3 +1,5 @@
 export const PRIVATE_API_TOKEN = process.env.PRIVATE_API_TOKEN;
 export const EVENTBRITE_API_BASE_URI = "https://www.eventbriteapi.com/v3";
-export const API_BASE_URI = "http://localhost:3000/api";
+export const API_BASE_URI = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL + "/api"
+  : "http://localhost:3000/api";

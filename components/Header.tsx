9b6e2cb70b9c10a11cffd9064f77c6d529fd9271
Asad Hayat <asadhayat.1068@gmail.com>
2023-10-25 14:38:17 +0500

    Login flow updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,8 @@ import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
+import { useAuth } from "@/app/Context/store";
+import App from "@/app/Web3Auth2";
 
 const navigation = [
   { name: "Dashboard", href: "/user" },
@@ -13,10 +15,14 @@ const navigation = [
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { isLoggedIn } = useAuth();
 
   return (
     <header className="bg-gray-900">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1 justify-center">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Robbie Williams Metaverse</span>
@@ -28,7 +34,9 @@ export default function Header() {
             />
             {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="" /> */}
           </a>
-          <h1 className="text-primary text-2xl font-semibold justify-center px-3 my-auto">Robbie Williams</h1>
+          <h1 className="text-primary text-2xl font-semibold justify-center px-3 my-auto">
+            Robbie Williams
+          </h1>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -41,24 +49,27 @@ export default function Header() {
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <Link key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
-              {item.name}
-            </Link>
-          ))}
+          {isLoggedIn &&
+            navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                {item.name}
+              </Link>
+            ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link
-            href="/login"
-            // TODO open modal.
-            // onClick={async () => login()}
-            className="text-sm bg-primary text-background p-3 rounded-xl font-semibold leading-6"
-          >
-            Log in <span aria-hidden="true">&rarr;</span>
-          </Link>
+          <App />
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
@@ -73,7 +84,11 @@ export default function Header() {
               <div className="text-primary font-lg">Robbie Williams</div>
               {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="" /> */}
             </Link>
-            <button type="button" className="-m-2.5 rounded-md bg-primary p-2.5 text-gray-400" onClick={() => setMobileMenuOpen(false)}>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md bg-primary p-2.5 text-gray-400"
+              onClick={() => setMobileMenuOpen(false)}
+            >
               <span className="sr-only">Close menu</span>
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
@@ -92,12 +107,9 @@ export default function Header() {
                 ))}
               </div>
               <div className="py-6">
-                <Link
-                  href="/login"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                >
+                <button className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">
                   Log in
-                </Link>
+                </button>
               </div>
             </div>
           </div>
